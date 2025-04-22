779a6aff60301c73acc5bdc3aa9c1ff60184e867
Artur-FE <arturfedoseiev@gmail.com>
2025-04-22 10:32:03 +0200

    home work 3

---
+++ home_work_2/task_2_1/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    private static final int N_Box = 1000;
+    private static final int CAPACITY = 1;
+
+    public static void main(String[] args) throws InterruptedException {
+        Warehouse warehouse = new Warehouse("#1");
+        Thread[] loaders = {
+                new Thread(new Loader("Jack", N_Box, CAPACITY, warehouse)),
+                new Thread(new Loader("John", N_Box, CAPACITY, warehouse)),
+                new Thread(new Loader("Nick", N_Box, CAPACITY, warehouse)),
+        };
+
+        for(Thread thread : loaders) {
+            thread.start();
+        }
+        for(Thread thread : loaders) {
+            thread.join();
+        }
+
+        System.out.println(warehouse);
+        System.out.println("Winner: " + Loader.getWinner());
+    }
+}
